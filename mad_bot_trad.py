300c80e34e363242655c56cca13544956907c907
Avishay Afik <avishay.afik@start.io>
2021-03-17 14:41:09 +0200

    fix: add

--- mad_bot_trad.py
+++ mad_bot_trad.py
@@ -29,8 +29,6 @@ logger.addHandler(ch)
 
 
 
-
-
 access_key = os.environ['access_key']
 secret_key = os.environ['secret_key']
 
@@ -67,7 +65,9 @@ close = []
 in_position = False
 
 
-candles = client.get_klines(symbol=market, interval=Client.KLINE_INTERVAL_1MINUTE)
+#candles = client.get_klines(symbol=market, interval=Client.KLINE_INTERVAL_1MINUTE)
+
+
 #print(candles)
 
 def ema():
@@ -77,7 +77,6 @@ def ema():
   ema_long = sum(ema_long_list) / len(ema_long_list)
   #logger.info(str(ema_long)+"  "+str(ema_short))
   return ema_long,ema_short
-
 
 
 
@@ -100,6 +99,42 @@ def wrightSellBuyCsv(Action,coin,Price,date):
 
 
 #candles = client.get_klines(symbol=market, interval=Client.KLINE_INTERVAL_5MINUTE)
+
+
+#########
+#f = open("results/results1111.csv", "a")
+f = open("results/test.csv", "a")
+
+#data_list = [["
+# "]]
+data_list = []
+data = client.get_klines(symbol=market, interval=Client.KLINE_INTERVAL_1MINUTE)
+for i in range(1, len(data)):
+    timestamp = str(data[i][0])
+    open = data[i][1]
+    high = data[i][2]
+    low = data[i][3]
+    close = data[i][4]
+    volume = data[i][5]
+    trim_timestamp = timestamp[:-3]
+    Date = (datetime.datetime.utcfromtimestamp(int(trim_timestamp)).strftime('%Y-%m-%d %H:%M:%S'))
+    list_to_append = [int(i),Date,open,high,low,close,volume]
+    data_list.append(list_to_append)
+    print(str(Date)+","+str(open)+","+str(high)+","+str(low)+","+str(close)+","+str(volume)+"\n")
+    f.write(str(Date)+","+str(open)+","+str(high)+","+str(low)+","+str(close)+","+str(volume)+"\n")
+f.close()
+
+    #print(Date)
+    #print(data[i])
+    #return data_list
+
+#print(data_list)
+data_list
+
+#with open('test.csv', 'w', newline='') as file:
+#    writer = csv.writer(file)
+#    writer.writerows(data_list)
+##############
 
 
 
